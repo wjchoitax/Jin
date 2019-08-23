c8542d410667f6016b8e7978dce3c0faa6690cad
wjchoitax <54171291+wjchoitax@users.noreply.github.com>
2019-08-23 17:23:04 +0900

    Add files via upload
    commit

---
+++ selenium-opinet.py
@@ -0,0 +1,38 @@
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import pandas as pd
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+
+driver = webdriver.Chrome('./chromedriver', options=options)
+driver.implicitly_wait(3)
+driver.get('http://www.opinet.co.kr/searRgSelect.do')
+time.sleep(2)
+driver.get('http://www.opinet.co.kr/searRgSelect.do')
+time.sleep(2)
+
+print('해당 사이트로 이동 완료..')
+
+sido_select = Select(driver.find_element_by_id("SIDO_NM0"))
+sido_select.select_by_visible_text('부산')
+time.sleep(1)
+
+sido_select = Select(driver.find_element_by_id("SIGUNGU_NM0"))
+sido_select.select_by_visible_text('해운대구')
+time.sleep(2)
+
+print('부산/해운대구 선택 완료..')
+driver.get_screenshot_as_file(' screen-shot.png')
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+href_value = soup.select_one('#body1 > tr:nth-child(2) > td.rlist > a')['href']
+print(href_value.split(',')[22][1:-1])
+
+
+driver.close()
---
+++ selenium001.py
@@ -0,0 +1,31 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+LOGIN_ID = 'wjchoi@ecredible.co.kr'
+LOGIN_PW = 'chldnrwls2#'
+
+driver = webdriver.Chrome('./chromedriver')
+driver.get('https://webfax.uplus.co.kr/main')
+
+time.sleep(2)
+
+driver.find_element_by_id('loginId').send_keys(LOGIN_ID)
+time.sleep(1.5)
+
+driver.find_element_by_id('loginPwd').send_keys(LOGIN_PW)
+time.sleep(1.5)
+driver.find_element_by_id('loginPwd').send_keys(Keys.RETURN)
+# driver.find_element_by_id('loginBtn').click()
+
+time.sleep(3)
+
+driver.find_element_by_id('recvInfo').click()
+
+# 웹페이지의 소스를 가져옴. ajax 요청 대응도 가능함.
+html = driver.page_source
+
+time.sleep(5)
+
+driver.close()
